74a73f8f549ff4c3e9a8873373b861521b4c1ba2
ldapunt <ldapunt@gmail.com>
2016-01-19 10:59:22 +0100

    config inicial

--- app/config.php
+++ app/config.php
@@ -1,6 +1,15 @@
 <?php
 	//Constantes
 	$configs = new HXPHP\System\Configs\Config;
+	
+	$configs->env->add('development');
+	$configs->env->development->baseURI ='/sistema/';
+	$configs->env->development->database->setConnectionData(array(
+		'host' => 'localhost',
+		'user' => 'root',
+		'password' => '',
+		'dbname' => 'tandembr'
+		));
 
 	/*
 		//Globais
